e406da9fc05595a333ef44935264201a94387750
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2025-07-01 23:30:34 -0300

    feat: initial commit

---
+++ src/main/java/com/gabriel/loginapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gabriel.loginapp.repository;
+
+import com.gabriel.loginapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findByUsername(String username);
+}
---
+++ src/main/java/com/gabriel/loginapp/security/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.gabriel.loginapp.security;
+
+import com.gabriel.loginapp.model.User;
+import com.gabriel.loginapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+
+    @Autowired
+    private UserRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUserName(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority(user.getRole()))
+        );
+    }
+
+
+}
